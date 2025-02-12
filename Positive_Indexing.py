216f6314747043945459383245eb39ef827e1eac
Sourodipta Rudra <165405580+sourodipta123@users.noreply.github.com>
2025-02-12 09:54:18 +0530

    Update

---
+++ Positive_Indexing.py
@@ -0,0 +1,16 @@
+'''Here we are going to do positive indexing and find out the item in the list 
+
+Positive indexing starts from 0 upto the of item in the list 
+
+For example, here I have taken a list which is [34,98,'A',6.5]
+
+Here the positive indexing of the list is : 
+           Items : 34  98   A    6.5
+Positive Indexing: 0   1    2     3
+'''
+
+list=[23 , 'A' , 'Hello World' , 45.89]
+print(list[0])
+print(list[1])
+print(list[2])
+print(list[3])
