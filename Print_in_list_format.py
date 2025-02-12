3b30ba6b493a33c7da5de10433e0c950cdc9d8db
Sourodipta Rudra <165405580+sourodipta123@users.noreply.github.com>
2025-02-12 10:13:00 +0530

    Update

---
+++ Print_in_list_format.py
@@ -0,0 +1,8 @@
+''' Here we are going to print a list of integer numbers by Multiplying 2 to each and print the item in list form
+'''
+
+list=[23,20,68,45,100]
+list1=[]
+for i in list:
+    list1.append(i*2)
+print(list1)
