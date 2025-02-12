d2e4a8c230e376d7a067aaa674b6c98fcd48e5ce
Sourodipta Rudra <165405580+sourodipta123@users.noreply.github.com>
2025-02-12 10:00:42 +0530

    Update

---
+++ Negative_Indexing.py
@@ -0,0 +1,17 @@
+''' Here we are going to print the items of the list using negative indexing. 
+
+Negative indexing starts from the length of the list in negative upto -1
+
+For example: Let's take a list [56.98 , 'D' , 55]
+
+            Items: 56.98    D     55
+Negative Indexing:  -3     -2     -1
+
+'''
+
+
+list=[23 , 'A' , 'Hello World' , 45.89]
+print(list[-4])
+print(list[-3])
+print(list[-2])
+print(list[-1])
