c3bd2af9fd69f393fad5f731b7074afc97285da7
Sourodipta Rudra <165405580+sourodipta123@users.noreply.github.com>
2025-02-13 08:58:34 +0530

    Update Fabonnaci_Sequence.py

--- Fabonnaci_Sequence.py
+++ Fabonnaci_Sequence.py
@@ -1,11 +1,12 @@
 #Print Fabonnaci Series
 
 n=int(input("Enter the maximum value: "))
-x=0
-y=1
-z=0
-for i in range(n):
-    print(z)
-    x=y
-    y=z
-    z=x+y
+a=0
+b=1
+print(a)
+print (b)
+for i in range(1,n-1):
+    c=a+b
+    print(c) 
+    a=b
+    b=c
