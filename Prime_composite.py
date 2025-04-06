0fd930c0b199f707e56dd135761d9f69a81548db
Sourodipta Rudra <165405580+sourodipta123@users.noreply.github.com>
2025-04-06 12:25:43 +0530

    Create Prime_composite.py

---
+++ Prime_composite.py
@@ -0,0 +1,13 @@
+a=int(input("Enter a number: "))
+if a==1 or a==0:
+    print(a,"is neither prime nor composite")
+elif a==2:
+    print("Prime")
+else:
+    for i in range(2,a):
+        if (a%i==0) :
+            print(a,"is composite")
+            break
+        else:
+            print(a,"is prime")
+            break
